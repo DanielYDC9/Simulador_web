183aed62bac966680c439f57c7d4624e104f77a4
DanielYDC9 <97945493+DanielYDC9@users.noreply.github.com>
2024-11-19 15:33:19 +0100

    Update Paciente.cpp
    Añadí las funciones registrar_paciente y eliminar_paciente con mensajes en consol

--- src/Paciente.cpp
+++ src/Paciente.cpp
@@ -2,3 +2,11 @@
 #include <iostream>
 
 Paciente::Paciente(std::string nombre, int id) : nombre(nombre), id(id) {}
+
+void Paciente::registrar_paciente() {
+    std::cout << "Paciente registrado: " << nombre << std::endl;
+}
+
+void Paciente::eliminar_paciente() {
+    std::cout << "Paciente eliminado: " << nombre << std::endl;
+}
