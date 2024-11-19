834c128d6499013f6a32a5dbe6a0abe3d44204cb
DanielYDC9 <97945493+DanielYDC9@users.noreply.github.com>
2024-11-19 15:38:45 +0100

    Update Paciente.cpp

--- src/Paciente.cpp
+++ src/Paciente.cpp
@@ -10,3 +10,11 @@ void Paciente::registrar_paciente() {
 void Paciente::eliminar_paciente() {
     std::cout << "Paciente eliminado: " << nombre << std::endl;
 }
+
+void Paciente::modificar_paciente() {
+    std::cout << "Modificando datos de: " << nombre << std::endl;
+}
+
+void Paciente::mostrar_paciente() {
+    std::cout << "Paciente: " << nombre << ", ID: " << id << std::endl;
+}
