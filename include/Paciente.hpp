7fe06ca74f8656486d2fd965344b8145c1b6960a
DanielYDC9 <daniyoungdecastro@gmail.com>
2024-11-19 15:21:39 +0100

    Agregar definición de la clase Paciente con atributos privados: nombre, id e historial_clínico

---
+++ include/Paciente.hpp
@@ -0,0 +1,15 @@
+#ifndef PACIENTE_HPP
+#define PACIENTE_HPP
+
+#include <string>
+
+class Paciente {
+private:
+    std::string nombre;
+    int id;
+    std::string historial_clinico;
+
+
+};
+
+#endif
